882f9cb763b2973422b41ac1156154272bd3e04a
ito-mono <ito@monoworks.co.jp>
2024-12-06 01:45:20 +0000

    token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,26 @@
 'use client';
 
 import { LightMode } from '@mui/icons-material';
-import { Box, Input, Typography } from '@mui/joy';
+import { Box, Button, Input, Typography } from '@mui/joy';
 import { ChangeEvent, useState } from 'react';
 
-import { getAuthHeader } from '@/util/auth';
-import { getEnv } from '@/util/getEnv';
+import { ask } from '@/util/apiService';
+import { getToken } from '@/util/auth';
 
 export default function Home() {
   const [value, setValue] = useState('');
   const [aiResponse, setAiResponse] = useState('');
-  const [asking, setAsking] = useState(false);
+  const [isAsking, setIsAsking] = useState(false);
 
   return (
-    <Box
-      height='100dvh'
-      display='flex'
-      flexDirection='column'
-      p={4}
-      gap={10}
-      alignItems='center'
-    >
+    <Box display='flex' flexDirection='column' p={4} gap={10}>
       <Box display='flex' flexDirection='column' alignItems='center'>
         <LightMode sx={{ width: '5rem', height: '5rem' }} color='warning' />
         <Typography level='h3' p={2}>
           11月のお天気ボット
         </Typography>
       </Box>
-      <Box
-        width='100%'
-        display='flex'
-        flexDirection='column'
-        alignItems='center'
-        gap={2}
-      >
+      <Box display='flex' flexDirection='column' alignItems='center' gap={2}>
         <Input
           value={value}
           placeholder='11月の東京と銚子の天気のことなら何でも聞いて下さい'
@@ -43,41 +30,37 @@ export default function Home() {
           onKeyDown={(e) => {
             if (e.key === 'Enter') onEnter();
           }}
-          disabled={asking}
+          disabled={isAsking}
         />
         <Typography>{aiResponse}</Typography>
+        <Button onClick={test}>テスト用</Button>
       </Box>
     </Box>
   );
+
+  async function test() {
+    console.log(await getToken());
+    // console.log(window.location);
+  }
 
   function onChange(e: ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value);
   }
 
   async function onEnter() {
-    setAsking(true);
+    setIsAsking(true);
 
-    const apiBaseUrl = await getEnv('API_BASE_URL');
-    const authHeader = getAuthHeader();
-
-    const url = `${apiBaseUrl}/azure_openai/ask`;
-    const response = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        ...authHeader,
-      },
-      method: 'POST',
-      body: JSON.stringify({ text: value }),
-    });
+    const response = await ask({ text: value });
 
     if (!response.ok) {
       console.error('Failed to fetch');
-      setAsking(false);
+      setIsAsking(false);
       return;
     }
 
     const data = await response.json();
     console.log(data);
+    setIsAsking(false);
     setAiResponse(data.res.content);
   }
 }
--- src/util/getEnv.ts
+++ src/util/getEnv.ts
@@ -1,13 +1,13 @@
+// クライアントサイドでも環境変数を取得できるようにする関数
+// Azure上の環境変数はNEXT_PUBLIC_をつけていたとしてもクライアントサイドから参照できないため
+// クライアントサイドから呼ばれた場合は、サーバーサイドにNEXT_PUBLIC_のついた環境変数名の値を取得しにいく
 export async function getEnv(name: string) {
   const isServer = typeof window === 'undefined';
-  let value;
 
-  if (isServer) value = process.env[name];
+  if (isServer) return process.env[name];
   else {
     const response = await fetch(`/env?name=NEXT_PUBLIC_${name}`);
-    if (!response.ok) value = '';
-    value = await response.text();
+    if (!response.ok) return '';
+    return await response.text();
   }
-
-  return value;
 }
